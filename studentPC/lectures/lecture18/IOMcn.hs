bded8132be7af4fb89a9a1a0d7ca632deca152b7
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-03-12 12:01:53 +0900

    lecture 18: add IOMcn.hs

---
+++ studentPC/lectures/lecture18/IOMcn.hs
@@ -0,0 +1,33 @@
+module IOMcn (
+	IOMcn, runIOMcn, (>>>), arr, app,
+	putLine, getLine, getInt, isEven) where
+
+import Prelude hiding (getLine)
+import qualified Prelude
+import Control.Arrow hiding ((>>>), arr)
+import qualified Control.Arrow
+import Control.Applicative
+import Data.Time
+
+type IOMcn = Kleisli IO
+
+runIOMcn :: IOMcn () a -> IO a
+runIOMcn = (`runKleisli` ())
+
+(>>>) :: IOMcn a b -> IOMcn b c -> IOMcn a c
+(>>>) = (Control.Arrow.>>>)
+
+arr :: (a -> b) -> IOMcn a b
+arr = Control.Arrow.arr
+
+putLine :: IOMcn String ()
+putLine = Kleisli putStrLn
+
+getLine :: IOMcn () String
+getLine = Kleisli $ const Prelude.getLine
+
+getInt :: IOMcn () Int
+getInt = Kleisli $ const $ Prelude.getLine >>= readIO
+
+isEven :: IOMcn () Bool
+isEven = Kleisli $ const $ even . floor . utctDayTime <$> getCurrentTime
